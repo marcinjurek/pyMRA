fix likelihood calculation

pyMRA/MRAGraph.py
@@ -27,7 +27,8 @@ class MRAGraph(object):
         self.r = r
         self.locs = locs
         self.d = np.shape(locs)[1]
-        self.root = Node(None, 'r', locs, locs, M, J, r, critDepth, cov, obs, R)
+        obsM = np.matrix(obs) # make sure observations are a matrix; otherwise many operations will not work
+        self.root = Node(None, 'r', locs, locs, M, J, r, critDepth, cov, obsM, R)
         self.obs_inds = np.where(np.logical_not(np.isnan(obs)))[0]
 
 
@@ -36,8 +37,7 @@ class MRAGraph(object):
 
     def getLikelihood(self):
 
-        return(self.root.d, self.root.u)
-        #return self.root.d + self.root.u
+        return self.root.d + self.root.u
 
 
     
@@ -176,7 +176,7 @@ class MRAGraph(object):
         """
         plots basis functions
         """
-        cmaps = [plt.cm.Reds, plt.cm.Greens, plt.cm.YlOrBr, plt.cm.Purples, plt.cm.RdPu]
+        cmaps = [plt.cm.Blues, plt.cm.copper, plt.cm.Blues, plt.cm.copper]#plt.cm.Greys, plt.cm.YlOrBr, plt.cm.Purples, plt.cm.RdPu]
 
         ### 1D version
         if np.shape(self.locs)[1]==1 and self.M>0:
@@ -192,7 +192,7 @@ class MRAGraph(object):
                 ncol = Bm.shape[1]; offset=0.3*ncol
                 
                 ax = fig.add_subplot(self.M+1, 1, m+1)
-                ax.set_ylim(top=1.1)
+                #ax.set_ylim(top=1.1)
                 ax.set_xlim(np.min(self.locs), np.max(self.locs))
                 
                 # draw partition lines
@@ -210,7 +210,7 @@ class MRAGraph(object):
                 
                 if m<(len(B)-2):
                     ax.get_xaxis().set_visible(False)
-                ax.set_title("resolution: %d" % m, fontsize='large')
+                ax.set_title("resolution: %d" % m, fontsize='x-large')
 
             
             plt.tight_layout()
@@ -243,6 +243,9 @@ class MRAGraph(object):
                     im = ax.imshow(np.array(func).reshape((dim_x, dim_y)), vmax=1, vmin=-0.1, cmap="coolwarm")
                     ax.get_xaxis().set_visible(False)
                     ax.get_yaxis().set_visible(False)
+
+                plt.suptitle("resolution: %d" % m, fontsize="x-large")
+
                     
                 grid.cbar_axes[0].colorbar(im)
 

pyMRA/MRANode.py
@@ -341,7 +341,7 @@ class Node(object):
             self.k = np.linalg.inv(self.kInv)
         except:
             logging.critical("Problem with the knots!")
-            pdb.set_trace()
+            pdb.set_trace()    
         self.kC = np.linalg.cholesky(self.k)
 
         #self.B = self.B[order,:]
@@ -366,7 +366,8 @@ class Node(object):
 
 
             ################     proposed change to posterior calculations     ################
-            obsInds = np.isfinite(obs).ravel()
+            obsInds = np.array(np.isfinite(obs)).ravel()
+            
             H = np.matrix(np.eye(len(obs)))[obsInds,:]
             
             Rmat = np.matrix(R*np.eye(sum(obsInds)))
@@ -398,11 +399,14 @@ class Node(object):
         self.kTilInv = lng.inv(self.kTil)
 
         
+        
         #likelihood:
         if self.leaf:
 
-            self.u = -omg[self.res].T * self.kTilInv * omg[self.res] + HRinvObs.T * HRinvObs
-
+            self.u = -omg[self.res].T * self.kTil * omg[self.res] + obs[obsInds].T * (1/R) * obs[obsInds]
+            if np.max(np.shape(self.u))>1:
+                pdb.set_trace()
+            
             sgnTil, logdetTil = slogdet(self.kTilInv)
             sgn, logdet = slogdet(self.kInv)
             sgnR, logdetR = slogdet(Rmat)
@@ -415,7 +419,10 @@ class Node(object):
 
             for ch in self.children:
                 self.d += ch.d
-                self.u += ch.u
+                try:
+                    self.u += ch.u
+                except:
+                    pdb.set_trace()
 
 
         
@@ -471,6 +478,7 @@ class Node(object):
             self.mean[chInds,:] += ch.mean
             self.var[chInds] += ch.var
 
+            
         logging.debug("Node %s: finished calculating posterior" % self.ID)
 
 

pyMRA/tests/test-MRA.py
@@ -21,7 +21,7 @@ if __name__=='__main__':
 
     logging.basicConfig(format='%(asctime)s %(message)s', datefmt='%H:%M:%S',level=logging.INFO)
 
-    #np.random.seed(12)
+    #np.random.seed(11)
 
     test_small = True
     diagnose = True
@@ -31,14 +31,14 @@ if __name__=='__main__':
     
     frac_obs = 0.4
     if test_small:
-        dim_x = 100
-        dim_y = 1
-        M=3; J=3; r0=2
-        critDepth = 5
+        dim_x = 20
+        dim_y = 20
+        M=3; J=2; r0=2
+        critDepth = M+1
     else:
         dim_x = 10
         dim_y = 10
-        M=3; J=2; r0=2
+        M=1; J=2; r0=2
         critDepth = 6
     
 
@@ -47,7 +47,7 @@ if __name__=='__main__':
     ### simulate data ###
 
     sig = 1.0
-    me_scale=1e-4
+    me_scale=1e-1
     kappa = 0.3
 
 
@@ -115,7 +115,7 @@ if __name__=='__main__':
     #sdP = np.flipud(sdP.reshape((dim_x, dim_y), order='A'))
     MRATime = time.time()-start
 
-    BP = MRATree.getBasisFunctionsMatrix(distr="posterior")
+    B = MRATree.getBasisFunctionsMatrix(distr="prior")
     
     logging.info('MRA predictions finished. It took {:.2}s'.format(MRATime))
     
@@ -149,28 +149,31 @@ if __name__=='__main__':
         SigP = np.linalg.inv(np.linalg.inv(Sig) + H.T * (1/R)*np.eye(len(obs_inds)) * H)
         sd = np.sqrt(np.diag(SigP))
 
-        y_obs[np.where(np.isnan(y_obs))] = 0
+        y_obs_k = np.zeros(np.shape(y_obs))
+        y_obs_k[np.where(np.isnan(y_obs))] = 0
 
-        xk = Sig*H.T*np.linalg.inv(varY)*H*y_obs
+        xk = Sig*H.T*np.linalg.inv(varY)*H*y_obs_k
         sdk = sd.reshape((dim_x, dim_y), order='A')
         #sdk = np.flipud(sd.reshape((dim_x, dim_y), order='A'))
         regTime = time.time() - start
 
         logging.info('Kriging finished. It took {:.2}s'.format(regTime))
   
-        # illustrate what simple kriging is about
-        # fig = plt.figure(figsize=plt.figaspect(0.2))
-        # axMean = fig.add_subplot(111)
-        # lineP = axMean.plot(locs, x, 'g-', linewidth=2, label='true state (X)')
-        # lineO = axMean.plot(locs[obs_inds], y[obs_inds], 'ko', markersize='7', label='observations (Y)')
+        #illustrate what simple kriging is about
+        fig = plt.figure(figsize=plt.figaspect(0.2))
+        axMean = fig.add_subplot(111)
+        lineO = axMean.plot(locs[obs_inds], y[obs_inds], color='#deaa87', marker='o', linestyle='None', markersize='7', label='observations (Y)')
+        lineP = axMean.plot(locs, x, color='#0000ff', linewidth=2, label='true state (X)')
 
-        # grid_ypos = np.min(np.vstack((np.min(x), y[obs_inds]))) - 0.1
-        # axMean.plot(locs, np.ones(len(locs))*grid_ypos, marker='o', markersize='7', linestyle='None', label='grid')
+
+        grid_ypos = np.min(np.vstack((np.min(x), y[obs_inds]))) - 0.1
+        axMean.plot(locs, np.ones(len(locs))*grid_ypos, marker='o', color='#000000', markersize='7', linestyle='None', label='grid')
         
-        # axMean.legend(loc=(0.6, 0.2), fontsize='x-large')
-        # plt.xticks(fontsize='x-large')
-        # plt.yticks(fontsize='x-large')
-        # plt.show()
+        axMean.legend(fontsize='x-large')
+        #axMean.legend(loc=(0.6, 0.2), fontsize='x-large')
+        plt.xticks(fontsize='x-large')
+        plt.yticks(fontsize='x-large')
+        plt.show()
 
 
 
@@ -182,20 +185,18 @@ if __name__=='__main__':
 
     if diagnose:
         #MRATree.drawBMatrix("prior")
-        MRATree.drawSparsityPat("prior")
+        #MRATree.drawSparsityPat("prior")
         #MRATree.drawBMatrix("posterior")
         #MRATree.drawSparsityPat("posterior")
         
         MRATree.drawBasisFunctions("prior")
         MRATree.drawBasisFunctions("posterior")
-        #MRATree.drawGridAndObs()
-        #MRATree.drawKnots()
+        MRATree.drawGridAndObs()
+        MRATree.drawKnots()
+
+
 
         
-    sys.exit(0)
-
-
-
 
 
     ### parameter optimization ###
@@ -244,12 +245,20 @@ if __name__=='__main__':
 
     if krig and compare:
         if dim_y>1:
-            mt.dispMat(xP.reshape((dim_x, dim_y)), cmap='RdYlBu', title="predicted x")
-            mt.dispMat(x.reshape((dim_x, dim_y)), cmap='RdYlBu', title="true x")
+
+
+            my_map = mpl.cm.get_cmap('Spectral')
+            my_map.set_bad(color='grey')
+
+            vmin = np.min(np.vstack((y_obs_k, x)))
+            vmax = np.max(np.vstack((y_obs_k, x)))
+            
+            mt.dispMat(xP.reshape((dim_x, dim_y)), cmap='RdYlBu', title="predicted x")          
+            mt.dispMat(x.reshape((dim_x, dim_y)), cmap='Spectral', title="true state (X)", vmax=vmax, vmin=vmin, fontsize='xx-large')
             if test_small:
                 mt.dispMat(xk.reshape((dim_x, dim_y), order='A'), cmap='RdYlBu', title='kriged x')
                 mt.dispMat((xP-xk).reshape((dim_x, dim_y)), cmap='coolwarm', title="predicted x - true x")
-            mt.dispMat(y_obs.reshape((dim_x, dim_y), order='A'), cmap='RdYlBu', title="observations")
+            mt.dispMat(y_obs.reshape((dim_x, dim_y), order='A'), cmap=my_map, title="observations (Y)", vmax=vmax, vmin=vmin, fontsize='xx-large')
             mt.dispMat(sdP, cmap='Reds', title="predicted sd")
 
         else:
@@ -264,6 +273,8 @@ if __name__=='__main__':
             axSd = fig.add_subplot(122)
             #axSd.plot(locs, sdP, 'r-')
             #axSd.plot(locs, sd, linestyle='dashed', color="gray")
-            axSd.plot(locs, sd-sdP.ravel(), 'r-')
+            axSd.plot(locs, sd-sdP.T.ravel(), 'r-')
             axSd.set_title("sd")
             plt.show()
+
+            pdb.set_trace()

pyMRA/tests/test-param-est.py
@@ -13,21 +13,21 @@ import pyMRA.MRATools as mt
 
 
 
-logging.basicConfig(format='%(asctime)s %(message)s', datefmt='%H:%M:%S',level=logging.DEBUG)
+logging.basicConfig(format='%(asctime)s %(message)s', datefmt='%H:%M:%S',level=logging.INFO)
 
-#np.random.seed(10)
+np.random.seed(10)
 
 ###### set simulation parameters #####
 frac_obs = 0.4 # the fraction of locations for which observations are available
-dim_x = 5; dim_y = 5
+dim_x = 30;  dim_y = 1
 
 sig = 1.0
-me_scale=0.2
-kappa = 0.1
+me_scale=1e-4
+kappa = 0.3
 
 
 ### MRA parameters
-M=0; J=3; r0=2
+M=2; J=3; r0=2
 critDepth = M+1
 
     
@@ -37,7 +37,7 @@ critDepth = M+1
 # generate the underlying process
 locs = mt.genLocations(dim_x*dim_y)
 
-Sig = sig*mt.Matern32(locs, l=kappa, sig=sig)
+Sig = mt.ExpCovFun(locs, l=kappa)
 SigC = np.matrix(lng.cholesky(Sig))
 
 x_raw = np.matrix(np.random.normal(size=(locs.shape[0],1)))
@@ -59,45 +59,46 @@ y_obs[obs_inds] = y[obs_inds]
 
 
 
+
 ##### parameter optimization #####
 
 def MRALikelihood(kappa):
     
-    cov = lambda _locs1, _locs2: mt.Matern32(_locs1, _locs2, l=kappa, sig=sig)
-    MRATree = MRAGraph(locs, M, J, r0, critDepth, cov, y_obs, me_scale)
+    cov = lambda _locs1, _locs2: mt.ExpCovFun(_locs1, _locs2, l=kappa)
+    MRATree = MRAGraph(locs, M, J, r0, critDepth, cov, y_obs, me_scale)        
     lik = MRATree.getLikelihood()
     return( lik )
 
 
 def TrueLikelihood(kappa):
 
-    cov = lambda _locs1: mt.Matern32(_locs1, _locs1, l=kappa, sig=sig)
+    cov = lambda _locs1: mt.ExpCovFun(_locs1, _locs1, l=kappa)
     obs = y_obs[obs_inds].ravel()
     obs_mat = np.matrix(obs).T
     obs_locs = locs[obs_inds,:]
-    Sig = cov(obs_locs) + np.eye(len(obs))*me_scale
+    varY = cov(obs_locs) + np.eye(len(obs))*me_scale
 
-    sign, logdet = np.linalg.slogdet(Sig)
-    full_hand_lik = -len(obs)*0.5*np.log(2*np.pi) - 0.5*logdet -0.5*(obs_mat.T*lng.inv(Sig)*obs_mat)
-    hand_lik = logdet + obs_mat.T*lng.inv(Sig)*obs_mat
     
-    model = mvn(mean=np.zeros(len(obs)), cov=Sig)
+    sign, logdet = np.linalg.slogdet(varY)
+    full_hand_lik = -len(obs)*0.5*np.log(2*np.pi) - 0.5*logdet -0.5*(obs_mat.T*lng.inv(varY)*obs_mat)
+    quad_form = obs_mat.T*lng.inv(varY)*obs_mat
+    hand_lik = logdet + quad_form
+    
+    model = mvn(mean=np.zeros(len(obs)), cov=varY)
     numpy_lik = model.logpdf(obs)
 
-    #logging.debug( "hand lik: %f" % hand_lik )
-    #logging.debug( "numpy lik: %f" % numpy_lik )
-    return( hand_lik )
+    return( logdet + quad_form )
 
 
 logging.info("MRA likelihood: %f" % MRALikelihood(kappa))
-logging.info("by-hand likelihood: %f" % TrueLikelihood(kappa))
+logging.info("true likelihood: %f" % TrueLikelihood(kappa))
 
 
 
-#xMRA = opt.minimize(MRALikelihood, [kappa], method='nelder-mead', options={'xtol':1e-1, 'disp':False})
-#xTrue = opt.minimize(TrueLikelihood, [kappa], method='nelder-mead', options={'xtol':1e-1, 'disp':False})
+xMRA = opt.minimize(MRALikelihood, [kappa], method='nelder-mead', options={'xtol':1e-1, 'disp':False})
+xTrue = opt.minimize(TrueLikelihood, [kappa], method='nelder-mead', options={'xtol':1e-1, 'disp':False})
 
-#logging.info("MRA estimate: %f" % xMRA.x[0])
-#logging.info("True estimate: %f" % xTrue.x[0])
+logging.info("MRA estimate: %f" % xMRA.x[0])
+logging.info("True estimate: %f" % xTrue.x[0])
 
         